4aeebca2f818b5b618c94cb8ea089f7ef8a26b48
DmytroShendryk <dimoon674@gmail.com>
2023-01-02 14:38:43 +0200

    hours to second

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let hour = prompt("How many hours?")
+function converHoursToSeconds (hour){
+    let seconds = (hour * 60) * 60;
+    return seconds
+}
+let result = alert((hour * 60) *60);
+
+console.log(hour);
+console.log(converHoursToSeconds);
+console.log(result);
